8c20540fbfd63e7208bc52ee049303ee26d33616
Nate Robinson <nrobinson@qslwm.com>
2015-12-25 11:28:42 -0600

    DB Dump and changes to Comment plugin

--- wp-content/plugins/cc-comment.php
+++ wp-content/plugins/cc-comment.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * Plugin Name: CC Comment Plugin
  * Description: Send a CC email to a particular email address when comment posted
  * Author: Nate Robinson
@@ -8,13 +7,43 @@
  * 
  */
 
-function cc_comment() {
-    global $_REQUEST;
-    $to = "nate@nate78.com";
-    $subject = "New Comment Posted at Your Blog" . $_REQUEST['subject'];
-    $message = "Message from " . $_REQUEST['name'] . "at email " . $_REQUEST['email'] .
-            ": \n" . $_REQUEST['comments'];
-    mail($to,$subject,$message);
-    
+function cc_comment($comment_ID) {
+    $comment = get_comment($comment_ID, ARRAY_A);
+    $to = get_option('cccomm_cc_email');
+    $subject = "New Comment Posted at Your Blog";
+    $message = "Here are the get_comment variables available to the function:\r\n";
+
+    foreach ($comment as $key => $value) {
+        $message .= "$key:  $value" . "\r\n";
+    }
+    //mail($to, $subject, $message);
+    wp_mail($to, $subject, $message);
 }
-add_action('comment_post','cc_comment()');
+
+add_action('comment_post', 'cc_comment');
+
+function cccomm_init() {
+    register_setting('cccomm_options', 'cccomm_cc_email');
+}
+add_action('admin_init', 'cccomm_init');
+
+function cccomm_option_page() {
+    ?>
+    <div class="wrap">
+        <h2>CC Comments Options</h2>
+        <p>Make selections here to define who gets CC'd when a post is submitted.</p>
+        <form action="options.php" method="POST" id="cc-comments-email-options-form">
+            <h3><label for="cccomm_cc_email">Email to CC:</label>
+            <input type="text" id="cc_email" name="cccomm_cc_email" value="<?php echo esc_attr(get_option('cccomm_cc_email'));?>" /></h3>
+            <p><input type="submit" name="submit" value="Save Email"></p>
+            <?php settings_fields('cccomm_options'); ?>
+        </form>
+    </div>
+    <?php
+}
+
+function cccomm_plugin_menu() {
+    add_options_page('CC Comments Option', 'CC Comments', 'manage_options', 'cc-comments-plugin', 'cccomm_option_page');
+}
+
+add_action('admin_menu', 'cccomm_plugin_menu');
--- wp-content/plugins/simple-widget.php
+++ wp-content/plugins/simple-widget.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * Plugin Name: Simple Widget
  * Version 1.0
  * Author: Nate Robinson
@@ -8,24 +7,58 @@
  */
 
 class SimpleWidget extends WP_Widget {
-    function SimpleWidget() {
-        
+
+    function __construct() {
+        parent::__construct(
+                'foo_widget', // Base ID
+                __('Widget Title', 'text_domain'), // Name
+                array('description' => __('A Foo Widget', 'text_domain'),) // Args
+        );
     }
-    
+
     function widget($args, $instance) {
-        extract( $args, EXTR_SKIP );
+        extract($args, EXTR_SKIP);
         $title = ($instance['title']) ? $instance['title'] : 'A Simple Widget';
-        $body = ($instance['body']) ? $instance['body'] : "A simple message";
-        
-        
-        
+        $body = ($instance['body']) ? $instance['body'] : 'A simple message';
+        ?>
+        <?php echo $before_widget; ?>
+        <?php echo $before_title . $title . $after_title; ?>
+        <p><?php echo $body; ?></p>
+        <?php
+            if(isset($instance)) :
+                foreach ($instance as $key => $value) {
+                    echo "$key: $value<br>";
+                }
+            endif;
     }
-    
+
     function update() {
         
     }
-    
+
     function form() {
-        
+        ?>
+        <label for="<?php echo $this->get_field_id('title');?>">
+        Title:
+        <input id="<?php echo $this->get_field_id('title');?>" 
+               name="<?php echo $this->get_field_name('title');?>"
+               value="<?php echo esc_attr($instance['title']);?>" />
+        </label><br>
+        <label for="<?php echo $this->get_field_id('body');?>">
+        Body:
+        <textarea id="<?php echo $this->get_field_id('body');?>" 
+               name="<?php echo $this->get_field_name('body');?>">
+               <?php echo esc_attr($instance['body']);?>
+               </textarea>
+        </label>
+        <?php
     }
+
 }
+
+function simple_widget_init() {
+    register_widget("SimpleWidget");
+}
+
+add_action('widgets_init', 'simple_widget_init');
+?>
